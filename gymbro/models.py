37d9eb03d7f18980ceae9edb680f8759f4cda788
Sotheary Ouk <sothearyo@gmail.com>
2020-03-24 14:58:14 -0700

    theary first commit - models set up

--- gymbro/models.py
+++ gymbro/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+# Workout Models
+class Workout(models.Model):
+    name = models.CharField(max_length=45)
+    weekday = models.CharField(max_length=15)
+    user = models.ForeignKey(User, related_name="workouts",on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=45)
+    weight = models.IntegerField()
+    workout = models.ForeignKey(Workout, related_name="exercises", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Set(models.Model):
+    rep =models.IntegerField()
+    weight = models.IntegerField()
+    rest = models.IntegerField()
+    exercise = models.ForeignKey(Exercise, related_name="sets", on_delete=models.CASCADE)
--- gymbro/urls.py
+++ gymbro/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     # This is the path and method to view the login and registration screen.
     path('', views.Reg_and_Login_index),
+    path('home', views.dashboard),
+    path('workout/<int:workout_id>', views.show_workout),
+    path('exercise/<int:exercise_id>', views.show_exercise),
+    path('myprofile', views.show_myprofile)
 ]
