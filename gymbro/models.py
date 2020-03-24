dad867a3ad9a50aa4a980c6ceccaad117728c00a
Christopher Holley <asiholley@gmail.com>
2020-03-24 16:16:06 -0700

    edit login form

--- gymbro/models.py
+++ gymbro/models.py
@@ -41,7 +41,6 @@ class User(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.CharField(max_length=30)
-    birthday = models.DateField()
     weight = models.IntegerField()
     password = models.CharField(max_length=30)
     created_at = models.DateTimeField(auto_now_add=True)
