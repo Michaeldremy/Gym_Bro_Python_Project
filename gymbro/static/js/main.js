0d8fb9d2dcf057fecbb0ceb042bdc636b5761190
Michael Remy <Michaeldremy@gmail.com>
2020-03-26 15:11:34 -0700

    Added validations to the login and register page. Also fix the login_validator in models.py and fixed the views.py login method.

--- gymbro/static/js/main.js
+++ gymbro/static/js/main.js
@@ -1,14 +1,50 @@
 $(document).ready(function(){
 
+const form = document.getElementById('register');
+const first_name = document.getElementById('form_first_name');
+const last_name = document.getElementById('form_last_name');
+const email = document.getElementById('form_email');
+const weight = document.getElementById('form_weight');
+const password = document.getElementById('form_password');
+const confirm_pw = document.getElementById('form_confirm')
+const errorElement = document.getElementById('error')
+
+form.addEventListener('submit', (e) => {
+	let messages = []
+	if (first_name.value.length < 3) {
+		messages.push('First name should be at least 3 characters')
+}
+
+	if (last_name.value.length < 3) {
+		messages.push('Last name should be at least 3 characters')
+}
+
+	if (password.value.length < 5){
+		messages.push('Password should be atleast 5 characters')
+	}
+
+	if (password.value != confirm_pw.value) {
+		messages.push('Passords must match')
+	}
+
+	if (messages.length > 0) {
+		e.preventDefault()
+		errorElement.innerText = messages.join(' and ')
+}
+})
+// The code ↓ runs the animation for sliding left and right.
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
+
 });
 
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
+
 });
 });
+
--- gymbro/views.py
+++ gymbro/views.py
@@ -73,7 +73,7 @@ def create_user(request):
     errors = User.objects.user_validator(request.POST)
     if len(errors) > 0:
         for key, value in errors.items():
-            messages.error(request, value)
+            messages.error(request, value, extra_tags=key)
         return redirect('/')
     else:
         fname = request.POST['form_first_name']
@@ -87,27 +87,22 @@ def create_user(request):
         return redirect("/home")
 
 def login(request):
-    if request.method=='POST':
+        user_login = User.objects.filter(email=request.POST['form_email']) 
+        # print(f"user login={user_login}")
+        if user_login: 
+            logged_user = user_login[0] 
+            # print(f"logged_user={logged_user}")
+            # print(bcrypt.checkpw(request.POST['form_password'].encode(), logged_user.password.encode()))
+            if bcrypt.checkpw(request.POST['form_password'].encode(), logged_user.password.encode()):
+                request.session['user_id'] = logged_user.id
+                # print("login succesful")
+                return redirect('/home')
         errors = User.objects.login_validator(request.POST)
         if len(errors) > 0:
             for key, value in errors.items():
-                messages.error(request, value)
-            print("error in login method is running")
+                messages.error(request, value, extra_tags=key)
+            # print("error in login method is running")
             return redirect('/')
-        user_login = User.objects.filter(email=request.POST['form_email']) 
-        print(f"user login={user_login}")
-        if user_login: 
-            logged_user = user_login[0] 
-            print(f"logged_user={logged_user}")
-            print(bcrypt.checkpw(request.POST['form_password'].encode(), logged_user.password.encode()))
-            if bcrypt.checkpw(request.POST['form_password'].encode(), logged_user.password.encode()):
-                request.session['user_id'] = logged_user.id
-                print("login succesful")
-                return redirect('/home')
-            else:
-                messages.error(request, "wrong password")
-                return redirect('/')
-    return redirect('/')
 
 
 # Post Requests for Workout
